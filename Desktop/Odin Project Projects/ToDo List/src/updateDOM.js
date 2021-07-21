9d93b3421f9a975e769af7a58e6c91c7b98397a2
DMP92 <77821396+DMP92@users.noreply.github.com>
2021-07-21 17:12:00 -0400

    Update updateDOM.js

--- Desktop/Odin Project Projects/ToDo List/src/updateDOM.js
+++ Desktop/Odin Project Projects/ToDo List/src/updateDOM.js
@@ -206,9 +206,11 @@ const tabSelection = (function () {
         // variables to retrieve both arrays - project and item
         const projects = JSON.parse(localStorage.getItem('projectArray'));
         const items = JSON.parse(localStorage.getItem('itemArray'));
-
+        
+        if ( projects != null ) {         
         const projectTasks = projects.map((a) => a.tasks);
-
+        } else {}
+            
         // whatever taks are dated for today show up in the DOM
         
         for ( var i = 0; i < items.length; i++ ) {
@@ -277,7 +279,10 @@ const tabSelection = (function () {
          const projects = JSON.parse(localStorage.getItem('projectArray'));
          const items = JSON.parse(localStorage.getItem('itemArray'));
  
-         const projectTasks = projects.map((a) => a.tasks);
+         if ( projects != null ) {         
+        const projectTasks = projects.map((a) => a.tasks);
+        } else {}
+            
         
          const year = new Date().getFullYear();
          const month = new Date().getMonth() + 1;
